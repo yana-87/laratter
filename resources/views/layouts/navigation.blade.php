ec47eb94346104fbdb40ea9d36f6d1d860e604f3
yana <ytourokuyouh@gmail.com>
2025-09-05 20:12:11 +0900

    viewファイルの作成、tweet作成処理と詳細画面の実装

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,13 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('ダッシュボード') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('tweets.index')" :active="request()->routeIs('tweets.index')">
+                        {{ __('Tweet一覧') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('tweets.create')" :active="request()->routeIs('tweets.create')">
+                        {{ __('Tweet作成') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -68,7 +74,13 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('ダッシュボード') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('tweets.index')" :active="request()->routeIs('tweets.index')">
+                {{ __('Tweet一覧') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('tweets.create')" :active="request()->routeIs('tweets.create')">
+                {{ __('Tweet作成') }}
             </x-responsive-nav-link>
         </div>
 
